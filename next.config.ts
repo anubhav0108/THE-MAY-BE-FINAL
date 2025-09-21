2b09201407f19c5f374c8664c9c4906071ab1f3a
Your Name <your.email@example.com>
2025-09-21 14:39:05 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
+  basePath: '/SIH-REPO',
   eslint: {
     ignoreDuringBuilds: true,
   },
